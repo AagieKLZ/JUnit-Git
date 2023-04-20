953587ac5659ad7db6d9dc77061262cf6e39abe1
AagieKLZ <darkyelan@gmail.com>
2023-04-20 08:41:14 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/junitgitm5/JUnitGitM5.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.junitgitm5;
+
+/**
+ *
+ * @author alumne
+ */
+public class JUnitGitM5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Math.sqrt(4);
+        System.out.println(Math.sqrt(2));
+    }
+}

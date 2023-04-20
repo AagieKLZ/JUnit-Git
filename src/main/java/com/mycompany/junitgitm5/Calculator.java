47f534184facfeb207f22a94b4132dc32b5738cd
A1bert04 <albertbru04@gmail.com>
2023-04-20 08:49:58 +0200

    Square root method and test added

--- src/main/java/com/mycompany/junitgitm5/Calculator.java
+++ src/main/java/com/mycompany/junitgitm5/Calculator.java
@@ -9,5 +9,11 @@ package com.mycompany.junitgitm5;
  * @author alumne
  */
 public class Calculator {
-    
+    /**
+     * @param number a number
+     * @return the square root of the number or -1 if the number is negative
+     */
+    public static double squareRoot(double number) {
+        return number < 0 ? -1 : Math.sqrt(number);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import com.mycompany.junitgitm5.Calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author alumne
  */
 public class CalculatorTest {
-    
+
     public CalculatorTest() {
     }
 
@@ -35,9 +36,22 @@ public class CalculatorTest {
     public void tearDown() throws Exception {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testSquareRoot() {
+        System.out.println("squareRoot");
+        double number = 4.0;
+        double expResult = 2.0;
+        double result = Calculator.squareRoot(number);
+        assertEquals(expResult, result, 0.0);
+
+        number = -4.0;
+        expResult = -1.0;
+        result = Calculator.squareRoot(number);
+        assertEquals(expResult, result, 0.0);
+
+        number = 0.0;
+        expResult = 0.0;
+        result = Calculator.squareRoot(number);
+        assertEquals(expResult, result, 0.0);
+    }
 }
